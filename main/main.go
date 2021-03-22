5a0b2abcf3164b25e8f4ca2274da7c84e4d3e49f
aixiangbing <373892393@qq.com>
2021-03-22 17:35:58 +0800

    feat(*) 搜索

--- main/main.go
+++ main/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"leetcode/leetcode/RandPrint"
+	"fmt"
+	//"leetcode/leetcode/RandPrint"
 )
 
 func main()  {
@@ -29,7 +30,48 @@ func main()  {
 	//randPrint := RandPrint.RandPrint{}
 	//randPrint.Print()
 
-	randNum := RandPrint.RandNum{}
+	//randNum := RandPrint.RandNum{}
+	//
+	//randNum.RandPrintNum()
 
-	randNum.RandPrintNum()
+	s := "2312312"
+	fmt.Println(s[1:2])
+}
+
+
+
+//  暴力解决
+
+func longestPalindromeThree(s string)  string {
+
+	// 定义字符串的长度
+	len := len(s)
+	if len < 2 {
+		return s
+	}
+
+	maxLen := 1
+	begin := 0
+
+	for i := 0; i < len - 1; i++ {
+		for j := i + 1; j < len; j++ {
+			if j - i + 1 > maxLen && validPalindromic(s, i, j) {
+				maxLen = j - i + 1
+				begin = i
+			}
+		}
+	}
+	return s[begin: begin + maxLen]
+}
+
+func validPalindromic(s string, left int, right int ) bool  {
+	for left < right {
+		if s[left] != s[right] {
+			return false
+		}
+
+		left++
+		right--
+	}
+	return true
 }
